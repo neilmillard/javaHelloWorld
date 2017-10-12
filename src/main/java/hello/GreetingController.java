ffdf4152694ae37e636562abde1fa67bf9dfab7c
neilmillard <neil@neilmillard.com>
2017-10-12 14:10:28 +0100

    Add RESTful files

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,14 +2,19 @@ package hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+// annotation shorthand for @Controller and @ResponseBody
+@RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    // This annotation can accept method=GET otherwise all requests are routed
+    // The response is marshalled into JSON by Jackson 2 (MappingJackson2HttpMessageConverter)
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
         return new Greeting(counter.incrementAndGet(),
